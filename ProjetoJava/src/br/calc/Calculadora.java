ba759a8acd6fc481ab26baca8e6be0fc8e2752a6
professor@utfpr <utfpr@utfpr>
2018-05-16 22:02:22 -0300

    adicionado projeto

---
+++ ProjetoJava/src/br/calc/Calculadora.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.calc;
+
+/**
+ *
+ * @author utfpr
+ */
+public class Calculadora {
+    public int somar(int a, int b){
+        return a + b;
+    }
+}
